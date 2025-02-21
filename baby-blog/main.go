502fabe216db8d70c9d9a6e1ed4dad33f955e251
Bryan Hernandez <2022156295@ub.edu.bz>
2025-02-21 17:56:06 -0500

    all handlers

--- baby-blog/main.go
+++ baby-blog/main.go
@@ -15,6 +15,23 @@ func week1Handler(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, "./templates/week1.html")
 }
 
+func week2Handler(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./templates/week1.html")
+}
+
+func week3Handler(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./templates/week1.html")
+}
+
+func week4Handler(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./templates/week1.html")
+}
+
+func week5Handler(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "./templates/week1.html")
+}
+
+
 func main() {
         mux := http.NewServeMux()
 
@@ -24,6 +41,10 @@ func main() {
         // Register the handlers
         mux.HandleFunc("/", homeHandler) 
         mux.HandleFunc("/week1", week1Handler)
+        mux.HandleFunc("/week2", week2Handler)
+        mux.HandleFunc("/week3", week3Handler)
+        mux.HandleFunc("/week4", week4Handler)
+        mux.HandleFunc("/week5", week5Handler)
         // Start listening for requests (start the web server)
         log.Println("Starting server on :4000")
         err := http.ListenAndServe(":4000", mux)
